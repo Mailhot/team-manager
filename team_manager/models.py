acbf3e1257c17a4d5097e6bccffe46a8730965e3
mailhot001 <fmailhot01@gmail.com>
2024-05-01 15:50:28 -0400

    implementing generate games

--- team_manager/models.py
+++ team_manager/models.py
@@ -1,4 +1,5 @@
-
+from datetime import datetime, timedelta
+import config
 
 
 class League():
@@ -10,13 +11,45 @@ class League():
 		self.lines = {}
 		self.games = {}
 		self.lines = {}
+		self.default_routing = [((0, 1), (2, 3)),
+								((0, 2), (1, 3)), 
+								((0, 3), (1, 2)), 
+								((2, 3), (0, 1)),
+								((1, 3), (0, 2)),
+								((1, 2), (0, 3)),
+								]
+
+
+	def generate_games(self, season, weekday, time, routing=None):
+		"""generate a series of game based on a season, day of week and time, line group instance"""
+		if routing == None:
+			routing == self.default_routing
+		# generate games dates from season.
+		start, end = season.start_date, season.stop_date
+		days = (start + timedelta(days=i) for i in range((end - start).days + 1))
+		l = [d for d in days if d.weekday() in [1] ]
+
+		k = 0
+		for date in l:
+			this_routing = routing[k]
+			team1 = Team(line1=this_routing[0][0], line2=this_routing[0][1], goaler=players[19], substitution=None)
+			team2 = Team(line1=this_routing[1][0], line2=this_routing[1][1], goaler=players[18], substitution=None)
+
+			game1 = Game(local_team=team1, visitor_team=team2, date=date, location=config.default_location, referee=None, season=season)
+			self.games[k] = game1
+			k += 1
+			# Restart loop if outside of routing length
+			if k > (len(routing) - 1):
+				k = 0
+
+
 
 class Season():
 	"""a season class"""
 	def __init__(self, start_date, stop_date, name):
 		super(Season, self).__init__()
-		self.start_date = start_date
-		self.stop_date = stop_date
+		self.start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+		self.stop_date = datetime.strptime(stop_date, '%Y-%m-%d').date()
 		self.name = name
 
 		self.games = {}
@@ -45,8 +78,8 @@ class Team():
 
 class Line():
 	"""a line class (3 offence and 2 defence)"""
-	def __init__(self, forward_left, forward_right, center, defense_left, defence_right):
-		super(Line, self).__init__()
+	def __init__(self, name, forward_left, forward_right, center, defense_left, defence_right):
+		self.name = name
 		self.forward_left = forward_left
 		self.forward_right = forward_right
 		self.center = center
--- team_manager/s24.py
+++ team_manager/s24.py
@@ -14,16 +14,20 @@ league_deschamps.players = players
 season_2024 = models.Season(name='2024-2025', start_date='2024-09-03', stop_date='2025-04-29')
 
 print('players', players)
-line1 = models.Line(forward_left=players[0], forward_right=players[1], center=players[2], defense_left=players[3], defence_right=players[4])
-line2 = models.Line(forward_left=players[5], forward_right=players[6], center=players[7], defense_left=players[8], defence_right=players[9])
-line3 = models.Line(forward_left=players[10], forward_right=players[11], center=players[12], defense_left=players[13], defence_right=players[14])
-line4 = models.Line(forward_left=players[15], forward_right=players[16], center=players[17], defense_left=players[18], defence_right=players[19])
+line1 = models.Line(name='Ligne 1', forward_left=players[0], forward_right=players[1], center=players[2], defense_left=players[3], defence_right=players[4])
+line2 = models.Line(name='Ligne 2', forward_left=players[5], forward_right=players[6], center=players[7], defense_left=players[8], defence_right=players[9])
+line3 = models.Line(name='Ligne 3', forward_left=players[10], forward_right=players[11], center=players[12], defense_left=players[13], defence_right=players[14])
+line4 = models.Line(name='Ligne 4', forward_left=players[15], forward_right=players[16], center=players[17], defense_left=players[18], defence_right=players[19])
 
+league_deschamps.lines = {0: line1, 1: line2, 2: line3, 3:line4}
 
+league_deschamps.generate_games(season=season_2024, weekday=1, time='20:00')
+
+# generate a game
 team1 = models.Team(line1=line1, line2=line2, goaler=players[19], substitution=None)
 team2 = models.Team(line1=line3, line2=line4, goaler=players[18], substitution=None)
 
-game1 = models.Game(local_team=team1, visitor_team=team2, date='2024-09-03', location='arena1', referee=None, season=season_2024)
+game1 = models.Game(local_team=team1, visitor_team=team2, date='2024-09-03', location=config.default_location, referee=None, season=season_2024)
 
 
 league_deschamps.games.append(game1)
